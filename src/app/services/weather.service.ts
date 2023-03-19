6fdf38e1d524d4db911128c710d4f1e3dd9f3405
riyanegi1211 <riyasnegi12@gmail.com>
2023-03-19 20:18:39 +0530

    SkyCast first commit

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  weatherApiBaseUrl:any
+
+  constructor(private http:HttpClient) { }
+
+    getWeatherData(cityName: string):Observable<WeatherData>
+    {
+      return this.http.get<WeatherData>(environment.weatherApiBaseUrl,{
+        headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostHeaderName,environment.XRapidAPIHostHeaderValue)
+        .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue),
+        params: new HttpParams()
+        .set('q',cityName)
+        .set('appid','da0f9c8d90bde7e619c3ec47766a42f4')
+        .set('units','metric')
+        .set('mode','json')
+      })
+      //GeoLocation
+    }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue:'openweather43.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue:'6fa2130d8fmsh374aa878009f44cp16b82ajsn799d76165db0',
+
+
+};
